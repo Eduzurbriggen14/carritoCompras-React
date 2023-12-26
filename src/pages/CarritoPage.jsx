6fb018555e9b61e1a618b15731d5e911e49daa42
Eduardo Zurbriggen <106617650+Eduzurbriggen14@users.noreply.github.com>
2023-12-26 18:18:09 -0300

    agregando limpiar localStorage y modificando viteconfig

--- src/pages/CarritoPage.jsx
+++ src/pages/CarritoPage.jsx
@@ -35,6 +35,7 @@ export const CarritoPage = () => {
     setMostrarForm(false);
     setMostrarModalCompraExitosa(true);
     vaciarCarrito();
+    localStorage.clear();
   };
 
   const handleCerrarCompraExitosa = () => {
