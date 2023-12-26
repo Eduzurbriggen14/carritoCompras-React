22182be9603bb0afdbba9aeb996b49b90fd19a65
Eduardo Zurbriggen <106617650+Eduzurbriggen14@users.noreply.github.com>
2023-12-26 17:42:06 -0300

    modificaciones

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base : "https://Eduzurbriggen14.github.io/carritoCompras-React"
+  base : "https://eduzurbriggen14.github.io/carritoCompras-React"
 })
